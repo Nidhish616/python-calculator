c68440e324324035537b66dc15e47919d7c7296b
Nidhish616 <nidhishsirwani@gmail.com>
2024-07-29 12:04:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+def add(x, y):
+    return x + y
+
+
+
+def subtract(x, y):
+    return x - y
+
+
+
+def multiply(x, y):
+    return x * y
+
+
+
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+
+    choice = input("Enter choice(1/2/3/4): ")
+
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+            break
+    else:
+        print("Invalid Input")
